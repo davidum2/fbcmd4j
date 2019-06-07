9c15c0b82836c2cb4e2df62a8d90e71f5584b7e5
David <davidum215@gmail.com>
2019-06-06 23:37:17 -0500

    se creo las NewsFeeds

--- src/fbcmd4j/Fbcmd4j.java
+++ src/fbcmd4j/Fbcmd4j.java
@@ -30,70 +30,63 @@ import facebook4j.auth.AccessToken;
 public class Fbcmd4j {
 
 	static final Logger logger = LogManager.getLogger(Facebook.class);//se establece la conexion con el log
-
 	private static String appId = "304222873824632";                      //id de facebook
 	private static String appSecret= "7ba79824a1f0505ec56a010b1f35623b";  //id secreto
-	private static Scanner scan;											//se establece un objeto del tipo escaner que sera utilizado por todo el programa
+	private static Scanner scan;										//se establece un objeto del tipo escaner que sera utilizado por todo el programa
 
 	
-	public static void main(String[] args) throws FacebookException {
-		// TODO Auto-generated method stub
-		
-		Facebook facebook = new FacebookFactory().getInstance(); //se crea el objeto facebook de la libreria Facebook
-		facebook.setOAuthAppId(appId, appSecret);                //se envia el id y el secret para el acceso
-		facebook.getOAuthAppAccessToken();							//se solicita el acceso por tooken
-		
-		try {
-			
-			scan = new Scanner(System.in);
-			while(true) {
-				
-				System.out.println("Bienvenido a Facebook \n\n"
-								+  "Opciones: \n"
-								+  "(1) NewsFeed \n"
-								+  "(2) Wall \n"
-								+  "(3) Publicar Estado \n"
-								+  "(4) Publicar Link \n"
-								+  "(5) Salir \n"
-								+  "\nPor favor ingrese una opción:");
-				try {
-						option = scan.nextInt();
-						scan.nextLine();
-						switch (option) {
-						
-					case 1:
-						
-						break;
-					case 2:
-						System.out.println("Mostrando Wall...");
-						System.out.println(facebook.getHome());
-						break;
-					case 3:
-						System.out.println("Escribe tu estado: ");
-						String estado = scan.nextLine();
-						facebook.postStatusMessage(estado);
-						break;
-					case 4:
-						System.out.println("Ingresa el link: ");
-						System.out.println("describa su link");
-						String descripcion = scan.nextLine();
-						System.out.println("ingrese la url de su link");
-						URL url = new URL(scan.nextLine()); 
-						facebook.postLink(descripcion, url);
-				
-												
-					case 5:
-						System.out.println("Gracias por usar el cliente!");
-						System.exit(0);
-						break;
-					default:
-						break;
+			public static void main(String[] args) throws FacebookException {
+				// TODO Auto-generated method stub
+					
+					Facebook facebook = new FacebookFactory().getInstance(); //se crea el objeto facebook de la libreria Facebook
+					facebook.setOAuthAppId(appId, appSecret);                //se envia el id y el secret para el acceso
+					facebook.getOAuthAppAccessToken();							//se solicita el acceso por tooken
+					
+					try {int option = 1;
+						scan = new Scanner(System.in);
+							while(true) {
+							
+								System.out.println("Bienvenido a Facebook \n\n"
+												+  "Opciones: \n"
+												+  "(1) NewsFeed \n"
+												+  "(2) Wall \n"
+												+  "(3) Publicar Estado \n"
+												+  "(4) Publicar Link \n"
+												+  "(5) Salir \n"
+												+  "\nPor favor ingrese una opción:");
+								try {
+										option = scan.nextInt();
+										scan.nextLine();
+										switch (option) { 
+										
+									case 1:
+										System.out.println("Mostrando NewsFeed...");
+										System.out.println(facebook.getActivities());
+										break;
+									case 2:
+										break;
+									case 3:
+										break;
+									case 4:
+										break;						
+									case 5:
+										break;
+									default:
+										break;
+										}
+									} catch (InputMismatchException ex) {
+										System.out.println("Ocurrió un errror, favor de revisar log.");
+										logger.catching(ex);
+									} catch (FacebookException ex) {
+										System.out.println("Ocurrió un errror, favor de revisar log.");
+										logger.catching(ex);
+									} catch (Exception ex) {
+										System.out.println("Ocurrió un error, favor de revisar log.");
+										logger.catching(ex);
+									}
+							}
+						} catch (Exception e) {
+							}
 			}
-	
-
-	} catch (Exception e) {
-
-
-	}
-	}
 }
+					
